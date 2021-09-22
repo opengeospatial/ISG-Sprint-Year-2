4b0e740f51dd689f1b1cde46bd77100aafd75321
Leonard Daly (Leonard Daly) <web3d@realism.com>
2021-09-21 19:46:42 -0700

    Final edits from Carl's comments.

--- er/buildDocs.bat
+++ er/buildDocs.bat
@@ -1,8 +1,14 @@
+rem Updated 2021-09-21 @ 19:44
 rem File for building HTML and PDF documents from AsciiDoc sources
 rem
 rem Build the HTML first
+
 asciidoctor -v -r asciidoctor-bibtex er.adoc
-rem
+
+rem ^^^^^ Above line builds HTML
+rem What follows builds other stuff
 rem Now build PDF
+
 asciidoctor-pdf -r asciidoctor-bibtex -a pdf-stylesdir=resources -a pdf-style=ogc -a pdf-fontsdir=resources/fonts er.adoc
+
 rem
